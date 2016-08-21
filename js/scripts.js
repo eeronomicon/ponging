928dee87e3fbf5569694d3eee1d00f38f19a2508
HK Kahng <hk@router-rooter.com>
2016-08-21 15:30:26 -0700

    add code to move ball and paddle

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,15 @@ $(document).ready (function(){
       },
       playground: {
         offsetTop: $("#playground").offset().top,
+        height: parseInt($("#playground").height()),
+        width: parseInt($("#playground").width())
+      },
+      ball: {
+        speed: 5,
+        x: 150,
+        y: 100,
+        directionX: 1,
+        directionY: 1
       }
     };
 
@@ -29,6 +38,7 @@ $(document).ready (function(){
     }
 
     function render() {
+      renderBall();
       renderPaddles();
       window.requestAnimationFrame(render);
     }
@@ -46,11 +56,58 @@ $(document).ready (function(){
     }
 
     function init() {
+      pingpong.timer = setInterval(gameloop, 1000/30);
       window.requestAnimationFrame(render);
       handleMouseInputs();
     }
+    function gameloop() {
+      moveBall();
+    }
+    function ballHitsTopBottom() {
+      var y = pingpong.ball.y + pingpong.ball.speed * pingpong.ball.directionY;
+      return y < 0 || y > pingpong.playground.height;
+    }
+    function ballHitsRightWall() {
+      return pingpong.ball.x + pingpong.ball.speed * pingpong.ball.directionX > pingpong.playground.width;
+    }
+    function ballHitsLeftWall() {
+      return pingpong.ball.x + pingpong.ball.speed * pingpong.ball.directionX < 0;
+    }
+    function playerAWin() {
+      // reset ball
+      pingpong.ball.x = 250;
+      pingpong.ball.y = 100;
+      // update ball location variables
+      pingpong.ball.directionX = -1;
+    }
+    function playerBWin() {
+      // reset ball
+      pingpong.ball.x = 150;
+      pingpong.ball.y = 100;
+      pingpong.ball.directionX = 1;
+    }
+    function moveBall() {
+      var ball = pingpong.ball;
+      if (ballHitsTopBottom()) {
+        ball.directionY *= -1;
+      }
+      if (ballHitsRightWall()) {
+        playerAWin();
+      }
+      if (ballHitsLeftWall()) {
+        playerBWin();
+      }
+      ball.x += ball.speed * ball.directionX;
+      ball.y += ball.speed * ball.directionY;
+    }
+    function renderBall() {
+      var ball = pingpong.ball;
+      $("#ball").css({
+        "left": ball.x + ball.speed * ball.directionX,
+        "top": ball.y + ball.speed * ball.directionY
+      });
+    }
 
-    // renderPaddles();
     init();
 
   }) (jQuery);
