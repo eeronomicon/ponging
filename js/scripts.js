25e502a2a1b146b2e7952715de89485abb83b914
HK Kahng <hk@router-rooter.com>
2016-08-21 14:54:34 -0700

    add graphic elements and initial bits of code and styling

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,55 @@
 // Front End Code
 $(document).ready (function(){
 
+  (function($) {
+    var pingpong = {
+      paddleA: {
+        x: 50,
+        y: 100,
+        width: 20,
+        height: 70
+      },
+      paddleB: {
+        x: 320,
+        y: 100,
+        width: 20,
+        height: 70
+      },
+      playground: {
+        offsetTop: $("#playground").offset().top,
+      }
+    };
+
+    function renderPaddles() {
+      $("#paddleB").css("top", pingpong.paddleB.y);
+      $("#paddleA").css("top", pingpong.paddleA.y);
+    }
+
+    function render() {
+      renderPaddles();
+      window.requestAnimationFrame(render);
+    }
+
+    function handleMouseInputs() {
+      $("#playground").mouseenter(function() {
+        pingpong.isPaused = false;
+      });
+      $("#playground").mouseleave(function() {
+        pingpong.isPaused = true;
+      });
+      $("#playground").mousemove(function(e) {
+        pingpong.paddleB.y = e.pageY - pingpong.playground.offsetTop;
+      });
+    }
+
+    function init() {
+      window.requestAnimationFrame(render);
+      handleMouseInputs();
+    }
+
+    // renderPaddles();
+    init();
+
+  }) (jQuery);
+
 });
